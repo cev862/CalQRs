881317303c5a123f5a0a60bcb8201248ef246a50
cev862 <108903931+cev862@users.noreply.github.com>
2022-12-15 17:52:28 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/a/~/macros/s/AKfycbxTRy1boOZbWaoxhUAivkHP2S-Cs9XlwPGsc_E3jHTSYW_1dshQv7wVPrl60TxcqSD4/exec";
