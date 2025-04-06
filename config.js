357101d58bfb5d1d3a207c55fe768716b3c5ca51
cev862 <108903931+cev862@users.noreply.github.com>
2025-04-06 18:06:23 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/a/~macros/s/AKfycbyMe5plxTyIEdct87rrPoqYghaxvrZr8GzuUvFA0HxcnenIQMJbA0SMjIoYyddIPTskRw/exec";
+const AS_URL_BASE = "https://script.google.com/a/~/macros/s/AKfycbyMe5plxTyIEdct87rrPoqYghaxvrZr8GzuUvFA0HxcnenIQMJbA0SMjIoYyddIPTskRw/exec";
